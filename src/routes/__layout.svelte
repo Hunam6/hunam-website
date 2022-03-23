477a5c0374b8e77eaf0b44f8bf4846a62c2110c4
Hunam <hunam@disroot.org>
2022-03-23 23:13:18 +0100

    support more screen sizes, better Goodgle description, more tweaks
    Signed-off-by: Hunam <hunam@disroot.org>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,115 +1,141 @@
 <script>
-	import Image from 'svelte-image';
-	import { onMount } from 'svelte';
-	import { gsap } from 'gsap';
-	onMount(() => {
-		if (!('ontouchstart' in document.documentElement)) {
-			const cursorDot = document.querySelector('.cursor-dot');
-			const cursorCircle = document.querySelector('.cursor-circle');
-			const nav = document.querySelector('nav');
+  import Image from 'svelte-image'
+  import { onMount } from 'svelte'
+  import { gsap } from 'gsap'
+  onMount(() => {
+    if (!('ontouchstart' in document.documentElement)) {
+      const cursorDot = document.querySelector('.cursor-dot')
+      const cursorCircle = document.querySelector('.cursor-circle')
+      const nav = document.querySelector('nav')
 
-			cursorDot.style.display = 'block';
-			cursorCircle.style.display = 'block';
-			gsap.to(cursorDot, {
-				scale: 1,
-				duration: 0.4
-			});
-			cursorCircle.style.backgroundColor = 'transparent';
+      cursorDot.style.display = 'block'
+      cursorCircle.style.display = 'block'
+      gsap.to(cursorDot, {
+        scale: 1,
+        duration: 0.4
+      })
+      cursorCircle.style.backgroundColor = 'transparent'
 
-			window.addEventListener('mousemove', cursor);
-			function cursor(e) {
-				gsap.to(cursorDot, {
-					x: Math.round(e.clientX - 3),
-					y: Math.round(e.clientY - 3),
-					duration: 0
-				});
-				gsap.to(cursorCircle, {
-					x: Math.round(e.clientX - 25),
-					y: Math.round(e.clientY - 25),
-					duration: 0.8
-				});
-			}
+      window.addEventListener('mousemove', cursor)
+      function cursor(e) {
+        gsap.to(cursorDot, {
+          x: Math.round(e.clientX - 3),
+          y: Math.round(e.clientY - 3),
+          duration: 0
+        })
+        gsap.to(cursorCircle, {
+          x: Math.round(e.clientX - 25),
+          y: Math.round(e.clientY - 25),
+          duration: 0.8
+        })
+      }
 
-			nav.addEventListener('mouseleave', () => {
-				gsap.to(cursorDot, {
-					scale: 1,
-					duration: 0.4
-				});
-				cursorCircle.style.backgroundColor = 'transparent';
-			});
-			nav.addEventListener('mouseover', () => {
-				gsap.to(cursorDot, {
-					scale: 0,
-					duration: 0.4
-				});
-				cursorCircle.style.backgroundColor = 'var(--yellow)';
-			});
-		}
-	});
+      nav.addEventListener('mouseleave', () => {
+        gsap.to(cursorDot, {
+          scale: 1,
+          duration: 0.4
+        })
+        cursorCircle.style.backgroundColor = 'transparent'
+      })
+      nav.addEventListener('mouseover', () => {
+        gsap.to(cursorDot, {
+          scale: 0,
+          duration: 0.4
+        })
+        cursorCircle.style.backgroundColor = 'var(--yellow)'
+      })
+    }
+  })
 </script>
 
 <header>
-	<span>
-		<Image src="/assets/favicon.png" alt="Hunam logo" lazy={false} wrapperClass="hunam-wrapper" />
-		<h2>Hunam</h2>
-	</span>
-	<nav>
-		<a href="/">/home</a>
-		<a href="/projects">/projects</a>
-		<a href="/posts">/posts</a>
-		<a href="/contact">/contact</a>
-	</nav>
+  <span>
+    <Image
+      src="/assets/favicon.png"
+      alt="Hunam logo"
+      lazy={false}
+      wrapperClass="hunam-wrapper"
+    />
+    <h2>Hunam</h2>
+  </span>
+  <nav>
+    <a href="/">/home</a>
+    <a href="/projects">/projects</a>
+    <a href="/posts">/posts</a>
+    <a href="/contact">/contact</a>
+  </nav>
 </header>
 <slot />
 
 <style>
-	header {
-		margin-top: 0.5rem;
-	}
+  header {
+    margin-top: 0.5rem;
+  }
 
-	span {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-	:global(.hunam-wrapper) {
-		margin-right: 2rem;
-		width: 5vh !important;
-		display: flex !important;
-		align-items: center;
-	}
-	h2 {
-		color: var(--white);
-		font-weight: 600;
-	}
+  :global(.hunam-wrapper) {
+    margin-right: 2rem;
+    width: 5vh !important;
+    display: flex !important;
+    align-items: center;
+  }
+  h2 {
+    color: var(--white);
+    font-weight: 600;
+  }
 
-	nav {
-		text-align: center;
-		margin-top: 1rem;
-	}
-	a {
-		text-decoration: none;
-		color: var(--yellow);
-		margin: auto 0.25rem;
-		font-size: 1.125rem;
-	}
+  nav {
+    text-align: center;
+    margin-top: 1rem;
+  }
+  a {
+    text-decoration: none;
+    color: var(--yellow);
+    margin: auto 0.25rem;
+    font-size: 1.125rem;
+  }
 
-	@media (min-width: 768px) {
-		header {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			gap: 2rem;
-			margin-top: 2rem;
-		}
+  @media (min-width: 768px) {
+    header {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+      margin-top: 2rem;
+    }
 
-		nav {
-			margin-top: 0;
-			padding-left: 1rem;
-		}
-		a {
-			padding: 1rem;
-		}
-	}
+    :global(.hunam-wrapper) {
+      width: 7.5vh !important;
+    }
+    h2 {
+      font-size: 2rem;
+    }
+
+    nav {
+      margin-top: 0;
+      padding-left: 1rem;
+    }
+    a {
+      padding: 1rem;
+      font-size: 1.25rem;
+    }
+  }
+
+  @media (min-width: 1441px) {
+    :global(.hunam-wrapper) {
+      width: 6.5vh !important;
+    }
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    a {
+      font-size: 1.125rem;
+    }
+  }
 </style>
