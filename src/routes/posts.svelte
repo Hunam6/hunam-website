74772d65118903cd65a66e67b173bc69a8f5cb6f
Hunam <hunam@disroot.org>
2022-04-23 12:19:39 +0200

    easter egg
    Signed-off-by: Hunam <hunam@disroot.org>

--- src/routes/posts.svelte
+++ src/routes/posts.svelte
@@ -1,3 +1,34 @@
+<script>
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    let solution = 'xuor draner'.split('')
+    let keyPressed = []
+
+    document.addEventListener('keydown', ev => {
+      keyPressed.push(ev.key)
+
+      // guessed correctly
+      if (solution.every((el, idx) => el === keyPressed[idx])) {
+        const title = document.getElementsByTagName('h1')[0]
+        title.innerHTML = "Joyeux anniversaire<br>à mon Papou que j'❤️!"
+        title.style.fontSize = '2rem'
+        title.style.color = 'var(--yellow)'
+        title.style.textShadow = 'none'
+        document.getElementById('heart-1').style.display = 'block'
+        document.getElementById('heart-2').style.display = 'block'
+        document.getElementById('yo').style.display = 'block'
+        // error
+      } else if (
+        keyPressed.length === solution.length ||
+        keyPressed[keyPressed.length - 1] !== solution[keyPressed.length - 1]
+      ) {
+        keyPressed = []
+      }
+    })
+  })
+</script>
+
 <svelte:head>
   <title>Hunam - Posts</title>
   <meta name="description" content="List of my blog posts." />
@@ -11,6 +42,9 @@
 </svelte:head>
 <main>
   <h1>SOON™</h1>
+  <p id="heart-1">❤️</p>
+  <p id="heart-2">❤️</p>
+  <p id="yo">🤙</p>
 </main>
 
 <style>
@@ -32,5 +66,28 @@
       font-size: 5rem;
       white-space: nowrap;
     }
+  }
+
+  #heart-1,
+  #heart-2,
+  #yo {
+    font-size: 10vw;
+    position: absolute;
+    display: none;
+  }
+  #heart-1 {
+    top: 20%;
+    left: 20%;
+    transform: rotate(-40deg);
+  }
+  #heart-2 {
+    top: 5%;
+    right: 15%;
+    transform: rotate(40deg);
+  }
+  #yo {
+    bottom: -10%;
+    right: 25%;
+    transform: scaleX(-1) rotate(-40deg);
   }
 </style>
