755f91b8e519b0161cc1e9dd3aada8f5ec0dceba
Hunam <hunam@disroot.org>
2023-06-20 01:04:31 +0300

    new website, using Qwik/QwikCity, add blogging system, add blog post

---
+++ src/root.tsx
@@ -0,0 +1,37 @@
+import { component$, useStyles$ } from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head";
+
+import globalStyles from "./global.css?inline";
+
+export default component$(() => {
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Dont remove the `<head>` and `<body>` elements.
+   */
+
+  useStyles$(globalStyles);
+
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <RouterHead />
+      </head>
+      <body
+        lang="en"
+        class="cursor-none bg-dark font-['Inconsolata_SemiBold'] selection:bg-yellow selection:text-dark"
+      >
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+      </body>
+    </QwikCityProvider>
+  );
+});
